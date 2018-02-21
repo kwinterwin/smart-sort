dc6bd91be447b4ec5fca6a426b04b75e5d425e35
Samuseva Anna <samuseva.a.s@mail.ru>
2018-02-21 16:16:15 +0300

    Intermediate variant

--- src/index.js
+++ src/index.js
@@ -1,26 +1,42 @@
 class Sorter {
+
   constructor() {
-    // your implementation
   }
 
   add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
+    var length=this.length;
+    this[length]=element;
+   }
+ 
+   at(index) {
+     return this[index];
+   }
+ 
+   get length() {
+       var count=0;
+       for(var i=0; this[i]!=undefined; i++){
+          count++;
+       }
+       return count;
+   }
+ 
+   toArray() {
+    var arr=[];
+    for(var i=0; i<this.length; i++){
+           arr[i] = this[i];
+       }
+       return arr;
+   }
 
   sort(indices) {
-    // your implementation
+   for(var i=0;i<indices.length;i++){
+           if (this[indices[i]]>=this[indices[i+1]])
+           {
+               var t=this[indices[i]];
+               this[indices[i]]=this[indices[i+1]];
+               this[indices[i+1]] =t;
+           }
+       }
   }
 
   setComparator(compareFunction) {
